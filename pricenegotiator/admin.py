d83d2d3fc4cce31d6226e2a76c161cfff4c7212c
ShrutikaDRaut <shrutika.raut@gmail.com>
2022-02-26 16:55:39 -0500

    db added

--- pricenegotiator/admin.py
+++ pricenegotiator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Product, Order, OrderDetail, User
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
+admin.site.register(User)
--- pricenegotiator/models.py
+++ pricenegotiator/models.py
@@ -1,3 +1,33 @@
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_Id = models.AutoField(primary_key=True, unique=True)
+    product_Name = models.CharField(max_length=100)
+    display_Price = models.IntegerField()
+    min_Price = models.IntegerField()
+    product_Img_url = models.URLField(max_length=200, null=True, blank=True)
+    model = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    product_Detail = models.TextField()
+    category_Name = models.CharField(max_length=100)
+
+class Order(models.Model):
+    order_Id = models.AutoField(primary_key=True, unique=True)
+    orderDetail_Id = models.IntegerField()
+    orderActual_Price = models.IntegerField()
+    orderFinal_Price = models.IntegerField()
+    user_Id = models.CharField(max_length=100)
+
+class OrderDetail(models.Model):
+    orderDetail_Id = models.AutoField(primary_key=True, unique=True)
+    order_Id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_Id = models.IntegerField()
+    product_Qty = models.IntegerField()
+
+class User(models.Model):
+    user_Id = models.AutoField(primary_key=True, unique=True)
+    first_Name = models.CharField(max_length=100)
+    last_Name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=75)
+    address = models.CharField(max_length=100)
