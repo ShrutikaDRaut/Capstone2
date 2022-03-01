3391c2bd0f5f80f0a323f8da54504b130a5f5ae4
Tejas <devanitejas23@gmail.com>
2022-02-28 20:01:31 -0500

    product data added into db

--- pricenegotiator/models.py
+++ pricenegotiator/models.py
@@ -1,16 +1,21 @@
-from statistics import mode
 from django.db import models
+
 
 class Product(models.Model):
     product_Id = models.AutoField(primary_key=True, unique=True)
     product_Name = models.CharField(max_length=100)
-    display_Price = models.IntegerField()
-    min_Price = models.IntegerField()
+    display_Price = models.CharField(max_length=10)
+    min_Price = models.CharField(max_length=10)
     product_Img_url = models.URLField(max_length=200, null=True, blank=True)
     model = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     product_Detail = models.TextField()
     category_Name = models.CharField(max_length=100)
+
+    def __str__(self):
+        """Model obj str representation"""
+        return self.product_Name
+
 
 class Order(models.Model):
     order_Id = models.AutoField(primary_key=True, unique=True)
@@ -19,11 +24,13 @@ class Order(models.Model):
     orderFinal_Price = models.IntegerField()
     user_Id = models.CharField(max_length=100)
 
+
 class OrderDetail(models.Model):
     orderDetail_Id = models.AutoField(primary_key=True, unique=True)
     order_Id = models.ForeignKey(Order, on_delete=models.CASCADE)
     product_Id = models.IntegerField()
     product_Qty = models.IntegerField()
+
 
 class User(models.Model):
     user_Id = models.AutoField(primary_key=True, unique=True)
--- pricenegotiator/views.py
+++ pricenegotiator/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-# Create your views here.
+from pathlib import Path
+from .models import Product
+import pandas as pd
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 def home(request):
-
+    total_products = Product.objects.all()
+    if len(total_products) == 0:
+        products = pd.read_csv(os.path.join(BASE_DIR, 'product.csv'))
+        for product in range(len(products)):
+            item = Product.objects.create()
+            item.product_Name = products.iloc[product, 1]
+            item.display_Price = products.iloc[product, 2]
+            item.min_Price = products.iloc[product, 3]
+            item.model = products.iloc[product, 4]
+            item.brand = products.iloc[product, 5]
+            item.product_Detail = products.iloc[product, 6]
+            item.category_Name = products.iloc[product, 7]
+            item.product_Img_url = products.iloc[product, 8]
+            item.save()
     return render(request, 'index.html')
