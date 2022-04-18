e86fb6acef92549b8a8b8d4a2ae92c5c86ec37e8
Tejas <devanitejas23@gmail.com>
2022-04-18 01:04:08 -0400

    Procfile added

--- WebApplication/gadgetbot/settings.py
+++ WebApplication/gadgetbot/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g^$fn9gshds^#jlx5p@!at=bm=&yyakdi^!nkp_un-#v(6rox$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gadgetbotai.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
