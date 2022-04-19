ea6ded07872255e930ac565a50ddc9e9844326e2
root <root@DESKTOP-6RU533L.localdomain>
2022-04-19 16:32:03 -0400

    Changed url

--- WebApplication/pricenegotiator/static/js/my-app/src/utils.js
+++ WebApplication/pricenegotiator/static/js/my-app/src/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function setSlots(convId, minPrice, maxPrice) {
-  const url = `http://localhost:5005/conversations/${convId}/tracker/events`;
+  const url = `http://68.183.198.98:5005/conversations/${convId}/tracker/events`;
   const response = await axios.post(url, [
     {
       event: "slot",
@@ -19,7 +19,7 @@ export async function setSlots(convId, minPrice, maxPrice) {
 }
 
 export async function sendMessage(message, convId) {
-  const url = "http://localhost:5005/webhooks/rest/webhook";
+  const url = "http://68.183.198.98:5005/webhooks/rest/webhook";
   const response = await axios.post(url, {
     sender: convId,
     message,
